a2b7c5288166f6d0da6b96a5d8a6b66d519193a9
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-08 16:40:55 +0300

    Sixth try

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -13,9 +13,9 @@ public class Animal {
     public void getDescription() {
         System.out.print("This animal is mostly " + color + ".");
         if (numberOfPaws > 0 && numberOfPaws > 1) {
-            System.out.print("It has " + (numberOfPaws) + " paws ");
+            System.out.print(" It has " + (numberOfPaws) + " paws ");
         } else if (numberOfPaws == 1) {
-            System.out.print("It has " + (numberOfPaws) + " paw ");
+            System.out.print(" It has " + (numberOfPaws) + " paw ");
         }
         if (hasFur) {
             System.out.print("and a fur.");
