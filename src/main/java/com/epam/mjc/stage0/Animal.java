84b41f304f108e35d8f7dc7854014cdfa910dc9a
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-08 17:31:39 +0300

    Seventh try

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,11 +1,14 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
-    private  String color;
-    private  int numberOfPaws;
+    private String color;
+    private int numberOfPaws;
     private boolean hasFur;
 
     Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
     }
     public void getDescription() {
         System.out.print("This animal is mostly " + color + ".");
